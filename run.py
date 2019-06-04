fd60ef222c88fb7322dbf1a6be5c51319e749a83
Jason Thomas <jason@lithiumfox.com>
2019-06-04 12:28:20 -0400

    Add a statsd test

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import logging.config
 import random
 import _thread
 import time
+import statsd
 
 cfg = {
     'version': 1,
@@ -28,6 +29,7 @@ cfg = {
 
 logging.config.dictConfig(cfg)
 logger = logging.getLogger('myfancyproject')
+c = statsd.StatsClient('localhost', 8125, prefix='foo')
 
 
 def load_words():
@@ -47,8 +49,18 @@ def run(words):
     logger.info(random_message(words, 10), extra=extra_fields)
 
 
+def emit_statsd():
+    c.incr('bar')
+    c.incr('foobar')
+    logger.info('emitted metric')
+
+
+# while True:
+#     words = load_words()
+#     for n in range(5):
+#         _thread.start_new_thread(run, (words,))
+#     time.sleep(.1)
 while True:
-    words = load_words()
     for n in range(5):
-        _thread.start_new_thread(run, (words,))
+        _thread.start_new_thread(emit_statsd, ())
     time.sleep(.1)
